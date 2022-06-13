e0dca1f10d2a07cce4248acb2c5b0ff7db139213
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-06-13 11:29:31 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   declarations: [
     AppComponent,
 
-    HomeComponent
+    HomeComponent,
 
     FooterComponent
 
