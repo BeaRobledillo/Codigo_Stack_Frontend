2dc45847d89212bd90152c722eae7e7a848a026c
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-06-13 14:41:51 +0200

    test bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
