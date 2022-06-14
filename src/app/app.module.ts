6965823f29f2cbdff2469b417417ef90ccdbd2f4
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-06-14 11:09:58 +0200

    add component header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UserComponent } from './user/user.component';
 import { ServiceComponent } from './service/service.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
@@ -26,6 +27,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     ServiceComponent,
     LoginComponent,
     NavbarComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
