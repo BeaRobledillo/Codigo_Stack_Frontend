49fc450e146ba7dadf56b2edf17fa6cac9a39de2
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-06-20 21:07:31 +0200

    add model user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PostdetailsComponent } from './postdetails/postdetails.component';
 
 const routes: Routes = [];
 const appRoutes = [
-  { path: '/login', component: LoginComponent,  pathMatch: 'full' }
+  { path: '/login', component: LoginComponent,  pathMatch: 'full' },
+  { path:'/post', component:PostdetailsComponent, pathMatch:'full' }
 ];
 
 @NgModule({
