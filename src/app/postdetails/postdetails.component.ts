205c0a993d41630af8c9ff97fe4bba8f672a034d
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-06-20 15:39:19 +0200

    component details

---
+++ src/app/postdetails/postdetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postdetails',
+  templateUrl: './postdetails.component.html',
+  styleUrls: ['./postdetails.component.scss']
+})
+export class PostdetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
