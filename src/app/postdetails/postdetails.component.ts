ac36a71d04770a60ee19fd3a80bdd737138c42b9
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-06-20 20:46:34 +0200

    logica postdetails

--- src/app/postdetails/postdetails.component.ts
+++ src/app/postdetails/postdetails.component.ts
@@ -1,42 +1,38 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import {
-  HttpErrorResponse
-} from '@angular/common/http';
-import {
-  Post
-} from '../model/post';
-import {
-  DataService
-} from '../service/data.service';
+import { Component,OnInit } from '@angular/core';
+import {HttpErrorResponse} from '@angular/common/http';
+import {Post} from '../model/post';
+import {DataService} from '../service/data.service';
 
+@Component( {
+    selector: 'app-postdetails',
+    templateUrl: './postdetails.component.html',
+    styleUrls: ['./postdetails.component.scss']
+  }
 
-@Component({
-  selector: 'app-postdetails',
-  templateUrl: './postdetails.component.html',
-  styleUrls: ['./postdetails.component.scss']
-})
-export class PostdetailsComponent implements OnInit {
-  post!: Post;
-  postList: Post[] = [];
+) export class PostdetailsComponent implements OnInit {
+  post !: Post;
+  postList: Post[]=[];
 
   constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getPosts();
+  }
 
   public getPosts(): void {
-    this.dataService.getAllPost().subscribe({
-      next: (response: Post[]) => {
-        console.log(response);
-        this.postList = response;
-        console.log(this.postList);
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    })
+    this.dataService.getAllPost().subscribe( {
+        next: (response: Post[])=> {
+          console.log(response);
+          this.postList=response;
+          console.log(this.postList);
+        }
 
+        ,
+        error: (error: HttpErrorResponse)=> {
+          alert(error.message);
+        }
+      }
+
+    )
   }
 }
