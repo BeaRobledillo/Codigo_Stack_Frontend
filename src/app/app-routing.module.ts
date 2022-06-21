2544f69a3552018bfd880f8783df06a2f65d9b71
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-06-21 12:16:27 +0200

    amago

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PostdetailsComponent } from './postdetails/postdetails.component';
 
 const routes: Routes = [];
 const appRoutes = [
-  { path: '/login', component: LoginComponent,  pathMatch: 'full' },
-  { path:'/post', component:PostdetailsComponent, pathMatch:'full' }
+  {path: 'home',component:HomeComponent},
+  { path: 'login', component: LoginComponent },
+  { path:'postdetails/:id', component:PostdetailsComponent }
 ];
 
 @NgModule({
--- src/app/postdetails/postdetails.component.ts
+++ src/app/postdetails/postdetails.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import {HttpErrorResponse} from '@angular/common/http';
 import {Post} from '../model/post';
 import {DataService} from '../service/data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component( {
     selector: 'app-postdetails',
@@ -10,29 +11,29 @@ import {DataService} from '../service/data.service';
   }
 
 ) export class PostdetailsComponent implements OnInit {
-  post !: Post;
-  postList: Post[]=[];
+ post!:Post;
+  postList:Post[]=[];
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService) {
+  }
 
   ngOnInit(): void {
-    this.getPosts();
+    this.getPostbyId();
+
   }
 
-  public getPosts(): void {
-    this.dataService.getAllPost().subscribe( {
-        next: (response: Post[])=> {
-          console.log(response);
-          this.postList=response;
-          console.log(this.postList);
-        }
-
-        ,
-        error: (error: HttpErrorResponse)=> {
-          alert(error.message);
-        }
+  public getPostbyId(): void{
+    this.dataService.getPostById().subscribe({
+      next: (response: Post[]) => {
+        console.log(response);
+        this.postList = response;
+        console.log(this.postList);
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
       }
-
-    )
+    })
   }
+
+
 }
