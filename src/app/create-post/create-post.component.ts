c01552352ac9c064d83405f0f767a0a6dd45f56d
aoliverasd <aoliverasdelso@gmail.com>
2022-06-24 10:40:20 +0200

    update

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { FooterComponent } from '../footer/footer.component';
+import { Router } from '@angular/router';
+import { DataService } from '../service/data.service';
+import { Post } from '../model/post';
 
 @Component({
   selector: 'app-create-post',
@@ -9,9 +10,47 @@ import { FooterComponent } from '../footer/footer.component';
 })
 export class CreatePostComponent implements OnInit {
 
-  constructor() { }
+    post: Post = {
 
-  ngOnInit(): void {
-  }
+      id: 1 ,
+      postName: '',
+      url: '',
+      description: '',
+      voteCount: 
+      
+    }
+    submitted: boolean | undefined;
 
+    constructor(private router: Router, private service:DataService) { }
+
+    ngOnInit(): void {
+    }
+
+    savePost(): void {
+      const data = {
+        id: this.post.id,
+        postName: this.post.postName,
+        url: this.post.url,
+        description: this.post.description,
+        voteCount: this.post.voteCount
+      };
+      this.service.createPost(data)
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this.submitted = true;
+          },
+          error: (e) => console.error(e)
+        });
+    }
+    newTutorial(): void {
+      this.submitted = false;
+      this.post = {
+        id: this.post.id,
+        postName: this.post.postName,
+        url: this.post.url,
+        description: this.post.description,
+        voteCount: this.post.voteCount
+      };
+    }
 }
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -16,13 +16,25 @@ import { Post } from '../model/post';
       private http: HttpClient
       ) { }
   
+      baseUrl = 'http://localhost:8080/api/posts/list';
   
       public getAllPost(): Observable<Post[]>{
     return this.http.get<Post[]>('http://localhost:8080/api/posts/list');
     }
 
     public getPostById(id:number): Observable<Post>{
-      return this.http.get<Post>(`${this.url}/${id}`);
+      return this.http.get<Post>(`${this.baseUrl}/${id}`);
+    }
+
+    createPost(post: Post) {
+      return this.http.post<Post>(this.url,post);
+    }
+
+    updatePost(id: any, data: any): Observable<any> {
+      return this.http.put(`${this.baseUrl}/${id}`, data);
+    }
+    deletePost(id: any): Observable<any> {
+      return this.http.delete(`${this.baseUrl}/${id}`);
     }
   }
 
